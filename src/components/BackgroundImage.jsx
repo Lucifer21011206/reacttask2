9ea1a08ee4528973bb99889ca04f3cb34f5547d9
Lucifer21011206 <104339478+Lucifer21011206@users.noreply.github.com>
2025-02-06 18:22:53 +0530

    Update BackgroundImage.jsx

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,20 +1,12 @@
 import { Box } from "@mui/material";
-import backgroundImage from "../images/landing_page_background.svg";
+// import backgroundImage from "../images/landing_page_background.svg";
+import styles from './styles.jsx'
+
 
 const BackgroundImage = () => {
   return (
     <Box
-      sx={{
-        position: "absolute",
-        right: 0,
-        top: 0,
-        width: "90%",
-        height: "100%",
-        zIndex: -1,
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
+      sx={styles.BackgroundImageBox}
     />
   );
 };
