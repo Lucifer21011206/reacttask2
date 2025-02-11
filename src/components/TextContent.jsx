4aa99c366cd2c77073a2bdfc32055471726cf91a
Lucifer21011206 <104339478+Lucifer21011206@users.noreply.github.com>
2025-02-11 11:28:31 +0530

    Add files via upload

---
+++ src/components/TextContent.jsx
@@ -0,0 +1,52 @@
+const TextContent = {
+    title : {
+        signup : "Create Your Account",
+        login : "Login Now",
+        otp : "OTP Verification",
+        accountcreatedtext : "Account Created" ,
+        accountcreated2 : "Complete Your Profile"
+    },
+
+    subtitle: {
+        signup: "To Get Started, We Need You To Create An Account With Us. It’s A Quick And Straightforward Process. That Will Only Take A Few Minutes.",
+        login : "To get started, log in with your account. It’s a quick and easy process that only takes a few minutes.",
+        otp: " Please enter the 6-digit verification code sent to 's123@gmail.com'. The code is valid for 3 minutes.",
+        accountcreatedtext: "Your account has been created successfully.",
+        remember:"Remember me",
+        forgot:"Forgot password?",
+        resend:"Resend",
+        time:"02:58"
+    },
+
+    placeholders: {
+        email: "Enter email address",
+        password: "Enter password",
+        confirmpassword: "Enter confirm password",
+        loginotp1:"Please enter OTP.",
+        loginotp2:"Please enter a complete 6-digit OTP."
+    },
+
+    termsandconditions: {
+        agree : "I agree to the",
+        tc : "Terms & Conditions",
+        and:"and",
+        pp:"Privacy Policy",
+        andconfirm:"and confirm that the country selection is correct"
+    },
+
+    btntext: {
+        signupbutton : "Create an account",
+        or : "OR",
+        signwithgoogle: "Sign in with Google",
+        already: "Already have an account?",
+        signuplogin: "Login",
+        login: "Login",
+        dont:"Don't have an account?",
+        signup:"Sign Up",
+        submit:"Submit",
+        uploadcv:"Upload your CV/Resume",
+        createcv:"Create your CV/Resume",
+        otpresent:"OTP Resent Successfully",
+    }
+} 
+export default TextContent
